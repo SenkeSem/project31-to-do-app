b10bc858a91606edefcdd9953c10700e112a55bf
SenkeSem <sen9morf@gmail.com>
2024-05-17 12:11:16 +0300

    added validation form on the Sign Up Page

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,13 +1,24 @@
-const Input = ({ label, type, placeholder }) => {
+const Input = ({ register, errors, label, type, placeholder }) => {
   return (
     <div className="mt-8">
       <label className="text-2xl font-medium">
         {label}
         <input
+          {...register(`${label}`, {
+            required: 'Поле обязательно к заполнению!',
+            minLength: {
+              value: 5,
+              message: 'Минимум 5 символов!',
+            },
+          })}
           type={type}
           className="w-full h-9 mt-3 border-b-2 border-b-inputGray font-normal text-base"
           placeholder={placeholder}
         />
+
+        {errors[`${label}`] && (
+          <p className="h-4 mt-2 text-sm text-btnRed">{errors[`${label}`].message || 'Error!'}</p>
+        )}
       </label>
     </div>
   );
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,25 +4,47 @@ import Input from '../components/shared/Input';
 import Button from '../components/shared/Button.jsx';
 
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const SignUpPage = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
     <div className="flex flex-col mt-4 pl-7 pr-5 pb-3">
       <ArrowLeft />
       <HeadingStartPages head={'Welcome'} text={'Sign up to continue'} />
       <img className="mx-auto mt-7" width={107} height={104} src="/circle.png" alt="circle" />
 
-      <form className="flex flex-col mt-7">
-        <Input label={'Username'} type={'email'} placeholder={'Enter your email'} />
-        <Input label={'Password'} type={'password'} placeholder={'Enter your password'} />
+      <form className="flex flex-col mt-7" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          register={register}
+          errors={errors}
+          label={'Username'}
+          type={'email'}
+          placeholder={'Enter your email'}
+        />
 
+        <Input
+          register={register}
+          errors={errors}
+          label={'Password'}
+          type={'password'}
+          placeholder={'Enter your password'}
+        />
         <Button
           className={
             'bg-btnRed w-full h-12 mt-16 italic text-lg font-thin text-signUpWhite rounded'
           }>
           Sign Up
         </Button>
-
         <Link to="/login" className="text-center">
           <Button className={'mt-12 text-lg italic font-bold text-btnRed'}>Sign In</Button>
         </Link>
