25bab9f0703994a9f2198558d03f7e93cd9f206b
SenkeSem <sen9morf@gmail.com>
2024-06-05 11:39:22 +0300

    added bigBlue Button for ProjectsPage

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,6 +2,7 @@ const Button = ({ children, type, disabled, ...props }) => {
   const buttonTypes = {
     primary: 'bg-btnRed w-full h-12 mt-16 italic text-lg font-thin text-signUpWhite rounded',
     secondary: 'mt-12 text-lg italic font-bold text-btnRed',
+    bigBlue: 'bg-todoBlue w-20 h-20 mt-6 rounded-lg font-thin text-2xl italic text-signUpWhite',
   };
 
   return (
--- src/components/shared/ProjectItem.jsx
+++ src/components/shared/ProjectItem.jsx
@@ -1,9 +1,16 @@
 const ProjectItem = ({ title, tasks, color }) => {
+  const itemType = {
+    blue: 'bg-todoBlue',
+    green: 'bg-projectGreen',
+    red: 'bg-projectRed',
+    purple: 'bg-projectPurple',
+  };
+
   return (
-    <div className="w-40 h-44 p-6 rounded-md shadow-md shadow-shadowGray flex-shrink-0">
+    <div className="w-full h-44 p-6 rounded-md shadow-md shadow-shadowGray">
       <div
-        className={`w-6 h-6 bg-${color} bg-opacity-40 rounded-full flex items-center justify-center`}>
-        <div className={`w-3 h-3 bg-${color} rounded-full`}></div>
+        className={`w-6 h-6 ${itemType[color]} bg-opacity-40 rounded-full flex items-center justify-center`}>
+        <div className={`w-3 h-3 ${itemType[color]} rounded-full`}></div>
       </div>
       <h5 className="mt-11 italic font-thin text-lg">{title}</h5>
       <p className="mt-3 font-medium text-base text-textGray">{tasks} Tasks</p>
